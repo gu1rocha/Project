6776cae00994c51a0634840f05803c926425b9e4
Guilherme Rocha <guirocha12395@gmail.com>
2022-08-24 00:31:31 -0300

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,55 @@
+const boxs = document.querySelector('.boxs')
+
+for (const key in textos) {
+    if (Object.hasOwnProperty.call(textos, key)) {
+        const element = textos[key];
+
+        const box = document.createElement('div');
+        box.classList.add(`${key}`,'box')
+        if(key !== 'apresentacao'){
+            box.classList.add('hidden')
+        }
+        box.style.backgroundImage = `url(./assets/img/${element.img.fundo})`
+        const cardContent = `
+                            <div class="top">
+                            <img src="./assets/img/${element.img.icon}" ${element.img.icon === 'barco.png' ? "class='barco'" : ''} alt="">
+                            <div>
+                                <h1>${element.titulo}</h1>
+                                <p>${element.paragrado}</p>
+                            </div>
+                            </div>
+                            <div class="bottom">
+                                <div class="boxbtn">
+                                    ${key === 'apresentacao' ? 
+                                    '<button class="next">NAVEGAR<span class="material-symbols-sharp nav">navigation</span></button>' :
+                                    key === 'agradecimento' ? '<button class="prev"><span class="material-symbols-sharp">arrow_back</span>VOLTAR</button>' :
+                                    '<button class="prev"><span class="material-symbols-sharp">arrow_back</span>VOLTAR</button><button class="next">PRÓXIMO<span class="material-symbols-sharp">arrow_forward</span></button>'
+                                }
+                                    
+                                </div>
+                            </div>
+                            `
+        box.innerHTML = cardContent;
+        boxs.appendChild(box);
+    }
+}
+
+const btnsNext = document.querySelectorAll('.next')
+
+for (const btn of btnsNext) {
+    btn.addEventListener('click',el =>{
+        el.path[3].classList.toggle('hidden')
+        el.path[3].nextElementSibling.classList.toggle('hidden')
+        window.scrollTo(0, 0);
+    })
+}
+
+const btnsPrev = document.querySelectorAll('.prev')
+
+for (const btn of btnsPrev) {
+    btn.addEventListener('click',el =>{
+        el.path[3].classList.toggle('hidden')
+        el.path[3].previousElementSibling.classList.toggle('hidden')
+        window.scrollTo(0, 0);
+    })
+}
---
+++ assets/js/textos.js
@@ -0,0 +1,66 @@
+const textos = {
+    apresentacao: {
+        titulo: "Embarque comigo nesta viagem!",
+        paragrado: "Vamos conhercer alguns animais!",
+        img:{
+            fundo: 'selva.png',
+            icon: 'reading.png'
+        }
+    },
+    primeiro: {
+        titulo: "Alpaca",
+        paragrado: "É um mamífero sul-americano estreitamente aparentado com a vicunha e, um pouco mais distante, com o guanaco e a lhama. A alpaca é um animal da família dos camelídeos.",
+        img:{
+            fundo: 'selva.png',
+            icon: 'alpaca.png'
+        }
+    },
+    segundo: {
+        titulo: "Vaca",
+        paragrado: "É uma espécie de bovino do gênero Bos e da ordem Artiodactyla. É um mamífero ungulado e apresenta dois dígitos (dedos) em cada membro.",
+        img:{
+            fundo: 'selva.png',
+            icon: 'cow.png'
+        }
+    },
+    terceiro: {
+        titulo: "Crocodilo",
+        paragrado: "Os crocodilos vivem nas Américas, África, Ásia e Austrália. A maioria dos crocodilos vivem nas margens de rios, enquanto os da Austrália e ilhas do Pacífico também frequentam o mar. Os crocodilos não possuem predadores naturais, por se tratar de um animal de topo na cadeia alimentar.",
+        img:{
+            fundo: 'selva.png',
+            icon: 'crocodile.png'
+        }
+    },
+    quarto: {
+        titulo: "Elefante",
+        paragrado: "É o termo genérico e popular pelo qual são denominados os membros da família Elephantidae, um grupo de mamíferos proboscídeos elefantídeos, de grande porte, do qual há três espécies no mundo atual, duas africanas (Loxodonta sp.) e uma asiática (Elephas sp.).",
+        img:{
+            fundo: 'selva.png',
+            icon: 'elephant.png'
+        }
+    },
+    quinto: {
+        titulo: "Cavalo",
+        paragrado: "É uma das duas subespécies existentes de Equus ferus. É um mamífero perissodáctilo pertencente à família taxonômica Equidae.",
+        img:{
+            fundo: 'selva.png',
+            icon: 'horse.png'
+        }
+    },
+    sexto: {
+        titulo: "Panda-gigante",
+        paragrado: "O panda-gigante foi descrito pelo missionário francês Armand David em 1869 como Ursus melanoleucus.[3] No ano seguinte, Alphonse Milne-Edwards ao examinar o material enviado por David, notou que os caracteres osteológicos e dentários o distinguia dos ursos e o aproximava ao panda-vermelho e aos procionídeos, descrevendo então um novo gênero para a espécie, e recombinando-a para Ailuropoda melanoleuca.",
+        img:{
+            fundo: 'selva.png',
+            icon: 'pandaBear.png'
+        }
+    },
+    agradecimento: {
+        titulo: "Agradecimentos",
+        paragrado: "Muito obrigado por participar comigo nesta viagem ao mundo dos animais.",
+        img:{
+            fundo: 'selva.png',
+            icon: 'videoCalling.png'
+        }
+    }
+}
